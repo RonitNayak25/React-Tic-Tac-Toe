43d72a02be6c23246ba718303c0a7c0194b1f389
Ronit Kumar Nayak <ronit@Ubuntu20.04>
2020-07-21 20:23:55 +0530

    Winner Status Added

--- src/components/Game.js
+++ src/components/Game.js
@@ -13,6 +13,13 @@ class Game extends React.Component {
         }
     }
 
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2 === 0)
+        });
+    }
+
     handleClick(i) {
         const history = this.state.history.slice(0, this.state.stepNumber+1);
         const current = history[history.length-1];
@@ -34,10 +41,33 @@ class Game extends React.Component {
     render() {
         const history = this.state.history;
         const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+        const moves = history.map((step, move) => {
+            const desc = move? "Go to #" + move : "Start The Game";
+            return (
+                <li key={move}>
+                    <button onClick={() =>{this.jumpTo(move)}}>
+                        {desc}
+                    </button>
+                </li>
+            );
+        });
+        let status;
+            if(winner) {
+                status = "Winner is  " + "\\" + winner + "/";
+            }
+            else {
+                status = "Next Player is " + (this.state.xIsNext ? 'X' : 'O');
+            }
+
         return(
             <div className="game">
                 <div className="game-board">
                     <Board onClick={(i) => this.handleClick(i)} squares={current.squares}/>
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
+                    <ul>{moves}</ul>
                 </div>
             </div>
         );
